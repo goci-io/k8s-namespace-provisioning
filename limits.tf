b011b907fd451e48ba9358772b4a05d02657f065
etwillbefine <tim.gatzemeier@gmx.de>
2019-08-16 18:01:59 +0200

    correct keys for resource quotas

--- limits.tf
+++ limits.tf
@@ -43,11 +43,11 @@ resource "kubernetes_resource_quota" "limits" {
   spec {
     hard = {
       "pods"                   = var.max_pods
-      "jobs.batch"             = var.max_jobs
       "persistentvolumeclaims" = var.max_pv_claims
-      "deployments.apps"       = var.max_deployments
-      "services.loadbalancers" = 0
-      "services.nodeports"     = 0
+      "count/jobs.batch"             = var.max_jobs
+      "count/deployments.apps"       = var.max_deployments
+      "count/services.loadbalancers" = 0
+      "count/services.nodeports"     = 0
     }
   }
 }
