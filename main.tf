10d5752ba6b23ba2ffa1df406199347d2385248a
etwillbefine <tim.gatzemeier@gmx.de>
2019-12-31 13:22:38 +0100

    add provider.tf to define terraform providers

--- main.tf
+++ main.tf
@@ -1,9 +1,3 @@
-terraform {
-  required_version = ">= 0.12.1"
-}
-
-provider "kubernetes" {
-}
 
 module "label" {
   source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.15.0"
