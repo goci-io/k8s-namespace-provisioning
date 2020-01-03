df6ec537a4ca6874ee0da929e40bf7def1042d86
etwillbefine <tim.gatzemeier@gmx.de>
2020-01-03 17:46:29 +0100

    add environment variable for additional options to describe different envs

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 
 module "label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.15.0"
-  namespace  = "${var.namespace}"
-  stage      = "${var.stage}"
-  attributes = "${var.attributes}"
-  name       = "${var.name}"
-  delimiter  = "-"
+  source      = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.15.0"
+  namespace   = var.namespace
+  stage       = var.stage
+  attributes  = var.attributes
+  environment = var.environment
+  name        = var.name
+  delimiter   = "-"
 }
