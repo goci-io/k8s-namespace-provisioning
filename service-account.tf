0b5c9a3606684ee63b0e1cfb2527b319424480b4
Tim Gatzemeier <etwillbefine@users.noreply.github.com>
2020-05-23 16:49:15 +0200

    feat: create service account specific rbac rules (#17)
    * feat: create service account specific rbac rules
    
    * run terraform fmt to pass checks

--- service-account.tf
+++ service-account.tf
@@ -1,23 +1,62 @@
-
-locals {
-  pull_secret_keys = keys(var.image_pull_secrets)
-}
-
-resource "kubernetes_service_account" "namespace" {
-  count = var.enabled_rbac_binding ? 1 : 0
+resource "kubernetes_service_account" "users" {
+  count = length(var.service_accounts)
 
   metadata {
-    name      = "${module.label.id}-apps"
+    name      = lookup(var.service_accounts[count.index], "name", "default")
     namespace = kubernetes_namespace.namespace.metadata.0.name
   }
 
   dynamic "image_pull_secret" {
-    for_each = var.image_pull_secrets
+    for_each = lookup(var.service_accounts[count.index], "image_pull_secrets", [])
 
     content {
       name = image_pull_secret.key
     }
   }
+}
+
+resource "kubernetes_role" "service_accounts" {
+  count = length(var.service_accounts)
+
+  metadata {
+    name      = lookup(var.service_accounts[count.index], "name", "default")
+    namespace = kubernetes_namespace.namespace.metadata.0.name
+  }
+
+  dynamic "rule" {
+    for_each = lookup(var.service_accounts[count.index], "rules", [])
+
+    content {
+      api_groups = rule.value.api_groups
+      resources  = rule.value.resources
+      verbs      = rule.value.verbs
+    }
+  }
+}
+
+resource "kubernetes_role_binding" "sa_binding" {
+  count = length(var.service_accounts)
+
+  metadata {
+    name      = lookup(var.service_accounts[count.index], "name", "default")
+    namespace = kubernetes_namespace.namespace.metadata.0.name
+  }
+
+  role_ref {
+    name      = element(kubernetes_role.service_accounts.*.metadata.0.name, count.index)
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "Role"
+  }
+
+  subject {
+    kind      = "ServiceAccount"
+    name      = element(kubernetes_service_account.users.*.metadata.0.name, count.index)
+    namespace = kubernetes_namespace.namespace.metadata.0.name
+  }
+}
+
+locals {
+  pull_secret_keys = keys(var.image_pull_secrets)
 }
 
 resource "kubernetes_secret" "image_pull" {
